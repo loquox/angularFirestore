076a39fa22f3f58354dc8dec1af66d0d9dc50a2a
loquox <crloquox@gmail.com>
2018-02-02 20:05:39 +0100

    segundo cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,6 +6,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+
 
 
 @NgModule({
@@ -14,7 +19,14 @@ import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
     AddTasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-fs'),
+
+
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5kVDv_uq4XK790EyAYJ_nxRUkZYjWu1o",
+    authDomain: "angular-fs-47948.firebaseapp.com",
+    databaseURL: "https://angular-fs-47948.firebaseio.com",
+    projectId: "angular-fs-47948",
+    storageBucket: "angular-fs-47948.appspot.com",
+    messagingSenderId: "103403661137"
+
+
+  }
 };
